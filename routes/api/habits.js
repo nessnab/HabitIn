7bc4f2954dac54e12d9358c27bc9b02aaf304318
nessnab <112866526+nessnab@users.noreply.github.com>
2026-04-20 16:45:05 +0700

    feat: add POST /api/habits

--- routes/api/habits.js
+++ routes/api/habits.js
@@ -13,6 +13,31 @@ router.get('/', requireAuth, async (req, res) => {
   }
 });
 
-// router.get
+// POST create a new habit
+router.post('/', requireAuth, async (req, res) => {
+  try {
+    const { title, goal, schedule, weeklyDay, customDays, time } = req.body;
+
+    if (!title) {
+      return res.status(400).json({ error: "Title is required" });
+    }
+    const newHabit = new Habit({
+      title,
+      goal,
+      schedule,
+      weeklyDay,
+      customDays,
+      time,
+      userId: req.user.id
+    });
+
+    const savedHabit = await newHabit.save();
+    res.status(201).json(savedHabit);
+  }
+  catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = router;
